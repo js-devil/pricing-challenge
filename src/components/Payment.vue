59ca03268e8ea455f39024bcddbb4645a2fde420
Oluwaseyi Joshua <seyijosh44@gmail.com>
2020-05-02 13:59:30 +0100

    Initial Commit

---
+++ src/components/Payment.vue
@@ -0,0 +1,180 @@
+<template>
+  <div class="container">
+    <div class="card card-outline-secondary" v-if="!success">
+      <div class="card-body">
+        <h3 class="text-center">Credit Card Payment</h3>
+        <hr />
+
+        <div class="alert alert-danger p-2 pb-3" v-if="errors.length">
+          <a
+            class="close font-weight-normal initialism"
+            @click="errors.length = 0"
+            data-dismiss="alert"
+            href="#"
+          >
+            <samp>×</samp>
+          </a>
+          <p v-for="error in errors" :key="error+Math.random()">- {{ error }}</p>
+        </div>
+
+        <form class="form" role="form" autocomplete="off">
+          <div class="form-group">
+            <label for="cc_name">Card Holder's Name</label>
+            <input
+              type="text"
+              class="form-control"
+              id="cc_name"
+              v-model="payment.name"
+              title="First and last name"
+              required="required"
+            />
+
+            <!-- pattern="\w+ \w+.*" -->
+          </div>
+          <div class="form-group">
+            <label>Card Number</label>
+            <input
+              type="text"
+              class="form-control"
+              v-model="payment.card_number"
+              autocomplete="off"
+              maxlength="20"
+              pattern="\d{16}"
+              title="Credit card number"
+              required
+            />
+          </div>
+          <div class="form-group row">
+            <label class="col-md-12">Card Exp. Date</label>
+            <div class="col-md-4">
+              <select class="form-control" v-model="payment.expiry_month" name="cc_exp_mo" size="0">
+                <option :key="n" v-for="n in 12">{{ twoDigits(n) }}</option>
+              </select>
+            </div>
+            <div class="col-md-4">
+              <select v-model="payment.expiry_year" class="form-control" name="cc_exp_yr" size="0">
+                <option>2018</option>
+                <option>2019</option>
+                <option>2020</option>
+                <option>2021</option>
+                <option>2022</option>
+              </select>
+            </div>
+            <div class="col-md-4">
+              <input
+                type="text"
+                class="form-control"
+                autocomplete="off"
+                maxlength="3"
+                pattern="\d{3}"
+                title="Three digits at back of your card"
+                required
+                v-model="payment.cvc"
+                placeholder="CVC"
+              />
+            </div>
+          </div>
+          <div class="row">
+            <label class="col-md-12">Amount</label>
+          </div>
+          <div class="form-inline">
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text">$</span>
+              </div>
+              <input
+                type="text"
+                class="form-control text-right"
+                id="exampleInputAmount"
+                :value="plan.price"
+                disabled
+              />
+              <div class="input-group-append">
+                <span class="input-group-text">.00</span>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div class="form-group row">
+            <div class="col-md-6">
+              <button
+                type="reset"
+                @click="$emit('closePayment')"
+                class="btn btn-default btn-lg btn-block"
+              >Cancel</button>
+            </div>
+            <div class="col-md-6">
+              <button
+                @click="makePayment"
+                type="submit"
+                class="btn btn-success btn-lg btn-block"
+              >Submit</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+    <Success v-if="success" :price="plan.price" :name="payment.name" />
+  </div>
+</template>
+
+<script>
+import Success from "@/components/Success.vue";
+export default {
+  components: { Success },
+  data() {
+    return {
+      payment: {},
+      errors: [],
+      success: false
+    };
+  },
+  props: {
+    plan: Object
+  },
+  methods: {
+    twoDigits: n => (String(n).length == 1 ? `0${n}` : n),
+    makePayment() {
+      const {
+        name,
+        card_number,
+        cvc,
+        expiry_year,
+        expiry_month
+      } = this.payment;
+      if (!name || name.length < 6) {
+        this.errors = [...this.errors, "name is required"];
+      }
+
+      if (
+        !card_number ||
+        card_number.length < 10 ||
+        isNaN(card_number.trim())
+      ) {
+        this.errors = [
+          ...this.errors,
+          "card number is required, must be at least 10 numbers"
+        ];
+      }
+
+      if (!cvc || cvc.length !== 3 || isNaN(cvc.trim())) {
+        this.errors = [...this.errors, "cvc is required, must be 3 numbers"];
+      }
+
+      if (!expiry_year || !expiry_month) {
+        this.errors = [...this.errors, "card expiration date is required"];
+      }
+
+      if (this.errors.length) return;
+      this.success = true;
+    }
+  }
+};
+</script>
+
+<style>
+.container {
+  width: 100%;
+  top: 50px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,229 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="background" v-if="!visible">
+      <div class="container">
+        <div class="panel pricing-table">
+          <div class="pricing-plan" v-for="({ title, icon, benefits, price }, i) in plans" :key="i">
+            <img :src="icon" alt class="pricing-img" />
+            <h2 class="pricing-header" @click="success = true">{{ title }}</h2>
+            <ul class="pricing-features">
+              <li
+                class="pricing-features-item"
+                v-for="benefit in benefits"
+                :key="benefit"
+              >{{ benefit }}</li>
+            </ul>
+            <span class="pricing-price">{{ price ? `$${price}` : "FREE" }}</span>
+            <button
+              @click="makePayment({ title, icon, benefits, price })"
+              :class="`pricing-button ${!price ? 'is-featured' : '' }`"
+            >{{ !price ? `Sign Up` : `Free Trial` }}</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <Payment v-if="visible" :plan="plan" @closePayment="visible = false; plan = {}" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Payment from "@/components/Payment.vue";
+import Success from "@/components/Success.vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  components: { Payment, Success },
+  data() {
+    return {
+      plans: [
+        {
+          title: "Personal",
+          icon: "https://s22.postimg.cc/8mv5gn7w1/paper-plane.png",
+          benefits: ["Instant Access", "Cancel any Time!", "Custom Domians"],
+          price: 0
+        },
+        {
+          title: "Small Team",
+          icon: "https://s28.postimg.cc/ju5bnc3x9/plane.png",
+          benefits: [
+            "All benefits in Personal",
+            "Higher Security",
+            "Multiple workers"
+          ],
+          price: 150
+        },
+        {
+          title: "Enterprise",
+          icon: "https://s21.postimg.cc/tpm0cge4n/space-ship.png",
+          benefits: [
+            "All benefits in Small Team",
+            "Faster Processors",
+            "Simple Scalability"
+          ],
+          price: 400
+        }
+      ],
+      visible: false,
+      plan: {}
+    };
+  },
+  methods: {
+    makePayment(plan) {
+      console.log(plan);
+      this.plan = plan;
+      this.visible = true;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.background {
+  padding: 0 25px 25px;
+  position: relative;
+  width: 100%;
+}
+
+.background::after {
+  content: "";
+  background: #60a9ff;
+  background: -moz-linear-gradient(top, #60a9ff 0%, #4394f4 100%);
+  background: -webkit-linear-gradient(top, #60a9ff 0%, #4394f4 100%);
+  background: linear-gradient(to bottom, #60a9ff 0%, #4394f4 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#60a9ff', endColorstr='#4394f4',GradientType=0 );
+  height: 350px;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
+  z-index: 1;
+}
+
+@media (min-width: 900px) {
+  .background {
+    padding: 0 0 25px;
   }
 }
-</script>
+
+.container {
+  margin: 0 auto;
+  padding: 50px 0 0;
+  max-width: 960px;
+  width: 100%;
+}
+
+.panel {
+  background-color: #fff;
+  border-radius: 10px;
+  padding: 15px 25px;
+  position: relative;
+  width: 100%;
+  z-index: 10;
+}
+
+.pricing-table {
+  box-shadow: 0px 10px 13px -6px rgba(0, 0, 0, 0.08),
+    0px 20px 31px 3px rgba(0, 0, 0, 0.09), 0px 8px 20px 7px rgba(0, 0, 0, 0.02);
+  display: flex;
+  flex-direction: column;
+}
+
+@media (min-width: 900px) {
+  .pricing-table {
+    flex-direction: row;
+  }
+}
+
+.pricing-table * {
+  text-align: center;
+  text-transform: uppercase;
+}
+
+.pricing-plan {
+  border-bottom: 1px solid #e1f1ff;
+  padding: 25px;
+}
+
+.pricing-plan:last-child {
+  border-bottom: none;
+}
+
+@media (min-width: 900px) {
+  .pricing-plan {
+    border-bottom: none;
+    border-right: 1px solid #e1f1ff;
+    flex-basis: 100%;
+    padding: 25px 50px;
+  }
+
+  .pricing-plan:last-child {
+    border-right: none;
+  }
+}
+
+.pricing-img {
+  margin-bottom: 25px;
+  max-width: 100%;
+}
+
+.pricing-header {
+  color: #888;
+  font-weight: 600;
+  letter-spacing: 1px;
+}
+
+.pricing-features {
+  color: #016ff9;
+  font-weight: 600;
+  letter-spacing: 1px;
+  margin: 40px 0 25px;
+}
+
+.pricing-features-item {
+  border-top: 1px solid #e1f1ff;
+  font-size: 12px;
+  line-height: 1.5;
+  padding: 15px 0;
+}
+
+.pricing-features-item:last-child {
+  border-bottom: 1px solid #e1f1ff;
+}
+
+.pricing-price {
+  color: #016ff9;
+  display: block;
+  font-size: 32px;
+  font-weight: 700;
+}
+
+.pricing-button {
+  border: 1px solid #9dd1ff;
+  border-radius: 10px;
+  color: #348efe;
+  display: inline-block;
+  margin: 25px 0;
+  outline: none !important;
+  padding: 15px 35px;
+  text-decoration: none;
+  cursor: pointer;
+  transition: all 150ms ease-in-out;
+}
+
+.pricing-button:hover,
+.pricing-button:focus {
+  background-color: #e1f1ff;
+}
+
+.pricing-button.is-featured {
+  background-color: #48aaff;
+  color: #fff;
+}
+
+.pricing-button.is-featured:hover,
+.pricing-button.is-featured:active {
+  background-color: #269aff;
+}
+</style>
